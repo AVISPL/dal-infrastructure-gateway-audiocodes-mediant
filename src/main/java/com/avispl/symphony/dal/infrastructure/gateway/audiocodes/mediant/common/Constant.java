d2b44c8ee756a60348dfa7e4dee2fdb6042858a0
HieuLM-AVISPL <hm15015@avispl.com>
2026-06-16 10:08:16 +0700

    SYAL-3068 - Implement Alarms Monitoring

--- src/main/java/com/avispl/symphony/dal/infrastructure/gateway/audiocodes/mediant/common/Constant.java
+++ src/main/java/com/avispl/symphony/dal/infrastructure/gateway/audiocodes/mediant/common/Constant.java
@@ -15,19 +15,31 @@ import lombok.experimental.UtilityClass;
 public class Constant {
 	//	Special characters
 	public static final String HASH = "#";
-	public static final String NONE_VALUE = "None";
-	public static final String NULL = "Null";
 	public static final String NOT_AVAILABLE = "N/A";
+	public static final String UNDERSCORE = "_";
+	public static final String SLASH = "/";
 
-	//	Groups
-	public static final String NETWORK_GROUP = "Network";
+	// Formats
+	public static final String PROPERTY_FORMAT = "%s#%s";
 
-	//	API
-	public final String STATUS_ENDPOINT = "/status";
+
+	//	Fail messages
+	public static final String LOGIN_FAILED = "Failed to login, please check the credentials";
+	public static final String FETCHED_DATA_NULL_WARNING = "Fetched data is null. Endpoint: %s, ResponseClass: %s";
+	public static final String FETCH_DATA_FAILED = "Exception while fetching data. Endpoint: %s, ResponseClass: %s";
+	public static final String CONVERT_DATA_FAILED = "Failed to convert response data to %s";
 
 	public static final String ADAPTER_METADATA = "AdapterMetadata";
 	public static final String ADAPTER_VERSION = "AdapterVersion";
 	public static final String ADAPTER_BUILD_DATE = "AdapterBuildDate";
 	public static final String ADAPTER_UPTIME_MIN = "AdapterUptime(min)";
 	public static final String ADAPTER_UPTIME = "AdapterUptime";
+	//	Groups
+	public static final String NETWORK_GROUP = "Network";
+
+	//	API
+	public final String STATUS_ENDPOINT = "/status";
+	public static final String ACTIVE_ALARMS_API = "/alarms/active";
+	public static final String ACTIVE_ALARM = "ActiveAlarms";
+
 }
